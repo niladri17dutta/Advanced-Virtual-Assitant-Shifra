8a1d490391f462a5075f8d06e51d83e20e2a14dd
niladri17dutta <niladridutta.2005@gmail.com>
2025-03-30 01:43:40 +0530

    `Refactor UI and functionality: updated title animation, CSS styles, and speech synthesis configuration`

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -12,19 +12,19 @@ function UserContext({children}) {
     text_speak.volume=1;
     text_speak.rate=1;
     text_speak.pitch=1;
-    text_speak.lang = 'hi-GB';
+    text_speak.lang = 'hi-GB'; // We can also use "en-US" or "en-IN" or "en-GB"
     window.speechSynthesis.speak(text_speak);
   }
   async function aiResponse(prompt){
     let text = await run(prompt)
-    let newText=text.split("**")&& text.split("*")&&text.split("*")&&text.replace("google","Niladri Dutta")&&text.replace("Google","Niladri Dutta");
-    // console.log(text);
-    setPrompt(newText);
-    speak(newText);
+    let newText=text.split("**")&&text.split("*")&&text.replace("google","Niladri Dutta")&&text.replace("Google","Niladri Dutta");
+    console.log(newText);
+    setPrompt(newText.split('.').slice(0, 2).join('.'));
+    speak(newText.split('.').slice(0, 2).join('.'));
     setResponse(true);
     setTimeout(()=>{
       setSpeaking(false);
-    },5000)
+    },15000)
   }
 
   let speechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -32,11 +32,26 @@ function UserContext({children}) {
   recognition.onresult=(e)=>{
     let currentindex=e.resultIndex;
     let transcript=e.results[currentindex][0].transcript;
-    // console.log(transcript);
+    console.log(transcript);
     setPrompt(transcript);
     aiResponse(transcript);
     // takeCommand(transcript.tolowerCase());
   }
+
+  // function takeCommand(command){
+  //   command=command.toLowerCase();
+  //   if(command.includes("open") && command.includes("youtube")){
+  //     window.open("https://www.youtube.com","_blank");
+  //     speak("Opening Youtube");
+  //     setResponse(true);
+  //     setPrompt("Opening Youtube.....");
+  //     setTimeout(() => {
+  //       setSpeaking(false);
+  //     }, 5000);
+  //   }else{
+  //     aiResponse(command);
+  //   }
+  // }
 
   let value={
     recognition,
--- src/gemini.js
+++ src/gemini.js
@@ -9,7 +9,7 @@ const generationConfig = {
     temperature: 1,
     topP: 0.95,
     topK: 40,
-    maxOutputTokens: 20,
+    maxOutputTokens: 100,
     responseMimeType: "text/plain",
 };
 
