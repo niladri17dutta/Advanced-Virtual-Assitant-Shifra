4ced1d4ac7e1fdc84595af24d9a08cc018462f98
niladri17dutta <niladridutta.2005@gmail.com>
2025-03-30 00:23:11 +0530

    Added functionality to the Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import "./App.css"
 import va from "./assets/ai.png"
 import { CiMicrophoneOn } from "react-icons/ci";
 import { datacontext } from './context/UserContext';
+import speakimg from "./assets/speak.gif";
+import aigif from "./assets/aiVoice.gif";
 
 function App() {
-  let {recognition}=useContext(datacontext)
+  let {recognition,speaking,setSpeaking,prompt,setPrompt,response,setResponse}=useContext(datacontext)
   
   return (
     <div>
       <div className="main">
         <img src={va} alt="" id="Shifra" />
         <span>I'm Shifra, Your Advanced Virtual Assitant</span>
+        {!speaking ?
         <button onClick={()=>{
+          setPrompt("Listening...");
+          setSpeaking(true);
+          setResponse(false);
           recognition.start();
         }}>Click Here <CiMicrophoneOn /></button>
+        :
+        <div className='response'>
+          {!response ?
+          <img src={speakimg} alt="" id="speak" />:<img src={aigif} alt="" className="aispeakq" />
+          }
+          <p>{prompt}</p>
+        </div>
+        }
       </div>
     </div>
   )
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,18 +3,28 @@ import run from '../gemini';
 export const datacontext=createContext();
 
 function UserContext({children}) {
+  let [speaking,setSpeaking]=React.useState(false);
+  let [prompt,setPrompt]=React.useState("Listening....");
+  let [response,setResponse]=React.useState(false);
+
   function speak(text){
     let text_speak=new SpeechSynthesisUtterance(text)
     text_speak.volume=1;
     text_speak.rate=1;
     text_speak.pitch=1;
-    text_speak.lang='hi-GB';
+    text_speak.lang = 'hi-GB';
     window.speechSynthesis.speak(text_speak);
   }
   async function aiResponse(prompt){
     let text = await run(prompt)
-    console.log(text);
-    speak(text);
+    let newText=text.split("**")&& text.split("*")&&text.split("*")&&text.replace("google","Niladri Dutta")&&text.replace("Google","Niladri Dutta");
+    // console.log(text);
+    setPrompt(newText);
+    speak(newText);
+    setResponse(true);
+    setTimeout(()=>{
+      setSpeaking(false);
+    },5000)
   }
 
   let speechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -22,13 +32,22 @@ function UserContext({children}) {
   recognition.onresult=(e)=>{
     let currentindex=e.resultIndex;
     let transcript=e.results[currentindex][0].transcript;
-    console.log(transcript);
+    // console.log(transcript);
+    setPrompt(transcript);
     aiResponse(transcript);
+    // takeCommand(transcript.tolowerCase());
   }
 
   let value={
-    recognition
+    recognition,
+    speaking,
+    setSpeaking,
+    prompt,
+    setPrompt,
+    response,
+    setResponse,
   }
+
   return (
     <div>
         <datacontext.Provider value={value}>
